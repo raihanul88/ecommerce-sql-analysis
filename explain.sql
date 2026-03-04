ed4efaf9487b2e7ce171f62cbfbb05d26c9da9c5
Raihanul Hasan. <raihanulhasan8889@gmail.com>
2026-03-05 00:20:37 +0600

    Create explain.sql

---
+++ explain.sql
@@ -0,0 +1,10 @@
+-- EXPLAIN
+SELECT 
+    c.name,
+    SUM(p.price * oi.quantity) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY c.name
+ORDER BY total_spent DESC;
